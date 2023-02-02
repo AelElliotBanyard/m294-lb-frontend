b283120476374474cc3b58f733a00b5e01b8fe58
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 08:55:18 +0000

    Deleted FieldComponent and adjusted the rest

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="callback($event)">
+      {{ label }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    onClick: {
+      type: Function,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    callback: function (e: Event) {
+      this.$emit("click", e);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -4,16 +4,36 @@
       <h1 class="text-2xl text-bold py-4">Login</h1>
     </div>
     <div id="fields" class="flex justify-center align-center flex-col">
-      <FieldComponent :field="email" type="text" label="E-Mail" />
-      <FieldComponent :field="password" type="password" label="Passwort" />
+      <div class="border-4 border-gray-500 relative w-min h-min m-4 rounded">
+        <p class="absolute bottom-7 left-2 px-2 bg-white cursor-default">
+          E-Mail
+        </p>
+        <input
+          type="text"
+          v-model="email"
+          class="h-10 w-64 px-2 focus-visible:outline-none"
+        />
+      </div>
+      <div class="border-4 border-gray-500 relative w-min h-min m-4 rounded">
+        <p class="absolute bottom-7 left-2 px-2 bg-white cursor-default">
+          Passwort
+        </p>
+        <input
+          type="password"
+          v-model="password"
+          class="h-10 w-64 px-2 focus-visible:outline-none"
+        />
+      </div>
     </div>
-    <div id="button"></div>
+    <div id="button">
+      <ButtonComponent :onClick="validateLogin" label="Anmelden" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import FieldComponent from "./FieldComponent.vue";
+import ButtonComponent from "./ButtonComponent.vue";
 
 export default defineComponent({
   name: "LoginComponent",
@@ -24,10 +44,12 @@ export default defineComponent({
     };
   },
   components: {
-    FieldComponent,
+    ButtonComponent,
   },
-  setup() {
-    return {};
+  methods: {
+    validateLogin() {
+      console.log(this.email, this.password);
+    },
   },
 });
 </script>
