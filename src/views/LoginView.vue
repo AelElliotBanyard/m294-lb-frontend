ab85fbd48ddaadee40c6d4599816299227c95493
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 07:26:56 +0000

    Login View created

---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <LoginComponent />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import LoginComponent from "@/components/LoginComponent.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  components: { LoginComponent },
+});
+</script>
