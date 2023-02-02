c4385501440b894626202b4f4f1486c4c0cb8b77
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 15:02:04 +0000

    finished Custom input

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,15 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ label }}</p>
+    <input
+      :type="inputType"
+      :value="modelValue"
+      @input="(event: Event | any) => $emit('ubdate:ModelValue', event.target.value)"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {
-    return {};
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: [String || Number],
+      required: true,
+    },
+    inputType: {
+      type: String,
+      required: false,
+      default: "Text",
+    },
   },
 });
 </script>
-
-<style scoped></style>
