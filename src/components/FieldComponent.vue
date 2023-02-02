a6aeec0e2e6b2ad3d548cf07833efb2aee1f054a
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 07:25:08 +0000

    Created custom input

---
+++ src/components/FieldComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ label }}</p>
+    <input :type="type" :v-model="field" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FieldComponent",
+  props: {
+    field: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
