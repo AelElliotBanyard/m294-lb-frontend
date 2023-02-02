5f15c77e3b5c893e8f984a15526727d3f5ed783d
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 20:21:17 +0000

    added NewTask to Navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/views/HomeView.vue";
 import EditView from "../views/EditView.vue";
 import DeleteView from "../views/DeleteView.vue";
+import NewTaskView from "../views/NewTaskView.vue";
+
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -28,7 +30,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/taskCreate",
     name: "createTask",
-    component: Home,
+    component: NewTaskView,
   },
 ];
 
