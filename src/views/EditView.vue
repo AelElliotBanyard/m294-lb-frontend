cdae2c02dbd3d1a3c07628b5790d91cdc182ca31
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 14:52:46 +0000

    added fetch to edit

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div v-if="!loading"></div>
 </template>
 
 <script lang="ts">
@@ -10,7 +10,20 @@ export default defineComponent({
     return {
       id: this.$route.params.id,
       loading: true,
+      task: {
+        id: 0,
+        completed: false,
+        title: "",
+      },
     };
+  },
+  created() {
+    fetch("http://zli.banyard.tech/task/" + this.id)
+      .then((response) => response.json())
+      .then((data) => {
+        this.task = data;
+        this.loading = false;
+      });
   },
 });
 </script>
