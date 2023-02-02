3d94a5fd8f98e7c1bde89c0ed640cf1162829a63
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 07:49:55 +0000

    Added Styling to Login component

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="border w-80">
     <div id="title">
-      <h1 class="text-2xl text-bold">Login</h1>
+      <h1 class="text-2xl text-bold py-4">Login</h1>
     </div>
-    <div id="fields">
+    <div id="fields" class="flex justify-center align-center flex-col">
       <FieldComponent :field="email" type="text" label="E-Mail" />
       <FieldComponent :field="password" type="password" label="Passwort" />
     </div>
