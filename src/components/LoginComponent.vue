e675941e72bb2e62106ee47cd5a0f4c0fb00dbd7
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 07:26:41 +0000

    Custom Login component

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id="title">
+      <h1 class="text-2xl text-bold">Login</h1>
+    </div>
+    <div id="fields">
+      <FieldComponent :field="email" type="text" label="E-Mail" />
+      <FieldComponent :field="password" type="password" label="Passwort" />
+    </div>
+    <div id="button"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FieldComponent from "./FieldComponent.vue";
+
+export default defineComponent({
+  name: "LoginComponent",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  components: {
+    FieldComponent,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
