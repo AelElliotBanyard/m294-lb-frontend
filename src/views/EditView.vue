e9e551d39256c9a0a85433d0dc8cb0aef79817e9
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-02 14:35:45 +0000

    Created View for edit

---
+++ src/views/EditView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      id: this.$route.params.id,
+      loading: true,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
