640252ea2df3e23aa976a63ea1baa1de6ad5f6f5
AelElliotBanyard <ael.banyard@gmail.com>
2023-02-03 13:48:49 +0000

    Removed custom alert

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,13 @@
 import { defineComponent } from "vue";
 import NavigationComponent from "./components/NavigationComponent.vue";
 import LoginComponent from "@/components/LoginComponent.vue";
+import CustomAlert from "@/components/CustomAlert.vue";
 
 export default defineComponent({
   components: {
     NavigationComponent,
     LoginComponent,
+    CustomAlert
   },
   data() {
     return {
